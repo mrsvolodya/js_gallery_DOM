00bea67e3274619dc81569497c01db59b259f6df
mrsvolodya <vovamursky@gmail.com>
2024-07-25 19:34:40 +0300

    make build

---
+++ index.f719d36d.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.addEventListener("DOMContentLoaded",function(){document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target.closest("a");n&&e.setAttribute("src",n.href)})});
+//# sourceMappingURL=index.f719d36d.js.map
